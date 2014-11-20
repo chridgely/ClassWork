1fadb391624abbc64621c0c8e9de2490ffb8f30b
Christopher Ridgley <cridgely@cs.loyola.edu>
2014-11-19 21:36:00 -0500

    Finished Append

--- Prolog_Assignment/append.pl
+++ Prolog_Assignment/append.pl
@@ -1,3 +1,5 @@
+/*This is my code -Chris Ridgely*/
+
 /*
 Purpose: Append Base Case
 In: Empty List, List, Result Variable 
@@ -37,14 +39,20 @@ notLast(X,Z) :-append(X, [_], Z).
 /*
 Purpose: If a list is a concatenation of three copies of the same sublist
 In: 2 Lists and Result Variable
-Out: User selected place
+Out: if it is true or false
 */
 isCat(X, A)  :- append(X,X,Y), append(X,Y,Z), Z == A .
 
 /*
 Purpose: Whether a list Y is formed by inserting an element A somewhere in a list X
-In: 2 Lists and Result Variable
-Out: User selected place
+In: 2 Lists and an element
+Out: Missing Number
 */
+isFormed(Y,A,X,Q,W,E) :- append(Y,A,X),append(Q,A,W),append(Q,E,Y). 
 
-
+/*
+Purpose: The other formed thing was just way too difficult to type
+In: A is the full list, B is the shorter list, C is the number to check
+Out: True or False and if no number is given, what number will evaluate to true
+*/
+howFormed(A,B,C) :- isFormed(_, _, A, _, B, C).
